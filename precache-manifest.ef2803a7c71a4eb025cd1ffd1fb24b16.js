94d4396af125fabb28ab3748351df4ad9fa2e3a6
reKrueger <64320105+reKrueger@users.noreply.github.com>
2021-02-19 19:53:43 +0100

    Updates

---
+++ precache-manifest.ef2803a7c71a4eb025cd1ffd1fb24b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4101d4c562ada04f7535adac0fd30033",
+    "url": "/archa.com/index.html"
+  },
+  {
+    "revision": "dc20011a79059c662441",
+    "url": "/archa.com/static/css/main.6b16dcbe.chunk.css"
+  },
+  {
+    "revision": "8ab4db22e0dd067b475c",
+    "url": "/archa.com/static/js/2.f88ecf8d.chunk.js"
+  },
+  {
+    "revision": "10fde5d258b94a4eee99e05097ecdab7",
+    "url": "/archa.com/static/js/2.f88ecf8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc20011a79059c662441",
+    "url": "/archa.com/static/js/main.ed3c550d.chunk.js"
+  },
+  {
+    "revision": "07a571648d68f95c10a8",
+    "url": "/archa.com/static/js/runtime-main.47f502f5.js"
+  }
+]);
