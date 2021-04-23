58060fda00dc55cb941c395380b5f3b913daa94a
reKrueger <64320105+reKrueger@users.noreply.github.com>
2021-04-23 16:58:46 +0200

    Updates

--- sw.js
+++ sw.js
@@ -39,13 +39,13 @@ function openPushNotification(event) {
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-/*
+
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
-*/
+
 self.addEventListener("push", receivePushNotification);
 self.addEventListener("notificationclick", openPushNotification);
 /*
